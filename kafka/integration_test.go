505d2dc2a00b6937331280686fae98caafc512e8
Duy <dnguyen@veritone.com>
2018-12-01 16:24:04 -0800

    Provide instruction to run test

--- kafka/integration_test.go
+++ kafka/integration_test.go
@@ -8,23 +8,27 @@ import (
 	"strconv"
 	"testing"
 
+	"github.com/stretchr/testify/assert"
 	"github.com/veritone/go-messaging-lib/kafka"
 )
 
+// Run test by `go test -v -tags=integration -run ^TestConsumerConnect`
+
 // Test whether consumer try to connect to all brokers. Verify by log
-func Test_ConsumerConnect(t *testing.T) {
+func TestConsumerConnect(t *testing.T) {
 	kafka.SetLogger(log.New(os.Stdout, "[sarama] ", log.LstdFlags))
 	multiBrokerSetup(t)
 	defer tearDown(t)
 
-	for j := 0; j < 10; j++ {
-		var brokers []string
-		firstPort := 9093
-		for i := 0; i < 15; i++ {
-			brokers = append(brokers, strconv.FormatInt(int64(i), 10))
-			brokers[i] = "kafka" + strconv.FormatInt(int64(i), 10) + ":" + strconv.FormatInt(int64(firstPort), 10)
-			firstPort++
-		}
-		kafka.NewConsumer("t1", "g1", kafka.WithBrokers(brokers...))
+	var brokers []string
+	port := 9093
+	for i := 0; i < 15; i++ {
+		brokers = append(brokers, strconv.FormatInt(int64(i), 10))
+		brokers[i] = "kafka" + strconv.FormatInt(int64(i), 10) + ":" + strconv.FormatInt(int64(port), 10)
+		port++
 	}
+	consumer, err := kafka.NewConsumer("t1", "g1", kafka.WithBrokers(brokers...))
+	assert.Nil(t, err, "should have no error")
+	err = consumer.Close()
+	assert.Nil(t, err, "should have no error")
 }
